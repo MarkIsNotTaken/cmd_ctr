2bd812a302b86ee23b896a007f1d1079724c8314
MarkIsNotTaken <mdemesillo@gmail.com>
2016-12-16 08:27:43 -1000

    app.js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function red(){
+	document.getElementById("box").style.backgroundColor = "red";
+}
+
+red();
+
+function blue(){
+	document.getElementById("box2").style.backgroundColor = "blue";
+}
+
+blue();
+
+function onmouse(){
+	document.getElementById("box3").style.backgroundColor = "black";
+}
+
+function ondbl(){
+	document.getElementById("box4").style.backgroundColor = "magenta";
+}
